abee7ed34b009df2496fbdefa08af9616a493678
kunsanglee <gosmdochee2@gmail.com>
2023-01-06 14:52:27 +0900

    Initial commit

---
+++ src/main/java/com/jscd/boutit/BoutitApplication.java
@@ -0,0 +1,13 @@
+package com.jscd.boutit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutitApplication.class, args);
+	}
+
+}
