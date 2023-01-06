7e0ceadbffd0f6cc54fcc982cdf3f57e38299ba2
imjaehy0b <121646578+imjaehy0b@users.noreply.github.com>
2023-01-06 16:02:37 +0900

    Add files via upload

---
+++ boutit-master/src/test/java/com/jscd/boutit/BoutitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jscd.boutit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoutitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
